2ac8171378d5c8138bd3480ad1b1a7d7ea1692c8
lisoviczp <phillip.lisovicz@gmail.com>
2015-10-25 23:54:23 -0400

    sunday edits

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,14 +1,14 @@
 class Artwork < ActiveRecord::Base
-	attr_accessor :artist
+	# attr_accessor :artist
 
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 	belongs_to :artist
 
-	def artist
-		self.artist
-	end
+	# def artist
+	# 	self.artist
+	# end
 
 	def artist_name
 		self.artist.first_name
